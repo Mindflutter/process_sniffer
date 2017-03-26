42111976706c942e7e06cd1764dd76537a543dc2
Igor Golyanov <igor.golyanov@f-secure.com>
2017-03-26 14:50:28 +0300

    added readme and docstrings

--- process_sniffer/helpers.py
+++ process_sniffer/helpers.py
@@ -3,11 +3,18 @@ import os.path as op
 
 
 def readable_timestamp(timestamp):
+    """ Get human readable timestamp from raw integer timestamp
+    :param timestamp: (int) raw timestamp
+    :return: (str) String in the format "2017-03-16 17:32:05"
+    """
     return datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
 
 
 def readable_size(size_in_bytes):
-    """ """
+    """ Get human readable size string from raw bytes
+    :param size_in_bytes: (int) size in bytes
+    :return: (str) String in the format "16.5 MB"
+    """
     for size_order in ['bytes', 'KB', 'MB', 'GB', 'TB']:
         if size_in_bytes < 1024.0:
             return "%3.1f %s" % (size_in_bytes, size_order)
@@ -15,7 +22,10 @@ def readable_size(size_in_bytes):
         
         
 def get_ports(connections):
-    # get ports from laddrs
+    """ Extract ports from psutil connections
+    :param connections: psutil.connections
+    :return: (list of int) local ports
+    """
     # to avoid duplicates initialize a set
     open_ports = set()
     for connection in connections:
@@ -24,6 +34,10 @@ def get_ports(connections):
 
 
 def get_file_info(process_path):
+    """ Get created, modified, filesize info on a given file
+    :param process_path: (str) File to retrieve info from 
+    :return: (dict) Basic info about the file
+    """
     if process_path:
         return {'created': readable_timestamp(op.getctime(process_path)),
                 'modified': readable_timestamp(op.getmtime(process_path)),
